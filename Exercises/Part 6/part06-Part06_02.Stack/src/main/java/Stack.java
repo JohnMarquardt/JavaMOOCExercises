1ecdb147885a71c9938ae210a81b646dc6dda334
JohnMarquardt <62271850+JohnMarquardt@users.noreply.github.com>
2020-08-26 21:24:23 -0500

    Added Part 6 Exercises 1 and 2

---
+++ Exercises/Part 6/part06-Part06_02.Stack/src/main/java/Stack.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Stack {
+    private ArrayList<String> things;
+    
+    public Stack() {
+        this.things = new ArrayList<>();
+    }
+    
+    public boolean isEmpty() {
+        if (things.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+    
+    public void add(String value) {
+        things.add(value);
+    }
+    
+    public ArrayList<String> values() {
+        return things;
+    }
+    
+    public String take() {
+        return things.remove(things.size() - 1);
+    }
+}
