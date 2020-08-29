5810bca310bfc9267b621ff24e679e72c2a4b0b3
JohnMarquardt <62271850+JohnMarquardt@users.noreply.github.com>
2020-08-28 22:43:26 -0500

    Preliminary work on Exercise 6:3
    Need to identify individual objects within the message ArrayList.

---
+++ Exercises/Part 6/part06-Part06_03.MessagingService/src/main/java/MessagingService.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class MessagingService {
+    
+    
+    public MessagingService() {
+        ArrayList<Message> message = new ArrayList<>();
+    }
+    
+    public void add(Message message) {
+        if (this.<Message> message.size() <= 279) {
+            this.message.add();
+        }
+    }
+    
+    public ArrayList<Message> getMessages() {
+        return this.message;
+    }
+}
