7fcf85e2f17a0a03d38d4e186ca216298632d4c1
JohnMarquardt <62271850+JohnMarquardt@users.noreply.github.com>
2020-09-12 10:24:24 -0500

    Began 6.7

---
+++ Exercises/Part 6/part06-Part06_07.HeightOrder/src/main/java/Room.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Room {
+    private ArrayList<Room> Person;
+    
+    public Room() {
+        this.Person = Person;
+    }
+    
+    public void add(Person person) {
+        this.add(person);
+    }
+    
+    public boolean isEmpty() {
+        if(this.ArrayList[1] == null) {
+            return true;
+        }
+        return false;
+    }
+    
+    public ArrayList<Person> getPersons() {
+        for(person i: Person) {
+            System.out.println(person);
+        }
+    }
+}
